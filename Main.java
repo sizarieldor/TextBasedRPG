b1f9f8017045d1d9f4d798bc724d0493269dc85e
sizarieldor <77972735+sizarieldor@users.noreply.github.com>
2021-12-25 01:35:36 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,21 +1,23 @@
 package com.company;
 
-import com.company.Actors.Monsters.Enemy;
+import com.company.Actors.Enemies.Demon;
+import com.company.Actors.Enemies.Enemy;
+import java.nio.file.Path;
 
 public class Main {
+    private static final Path MONSTER_DATA_PATH = Path.of("MonsterData.csv");
 
     public void main(String[] args) {
-
-
 
     }
 
 
     public static Enemy enemyBuilder(String enemyType) {
         Enemy newEnemy = null;
+        String[] newMonsterData = GameStatFilesReader.findMonsterData(enemyType,MONSTER_DATA_PATH);
         switch (enemyType) {
             case "Demon":
-
+                Enemy newDemon = new Demon();
                 break;
             case "Living":
                 break;
@@ -26,8 +28,4 @@ public class Main {
         return newEnemy;
     }
 }
-
-
-//public static void PrintState(int heroLevel){
-//    System.out.println("Hero level:" + heroLevel);
 
